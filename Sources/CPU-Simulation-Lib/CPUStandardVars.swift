4905e5d851de7e554863b2c78b2cf841f445d193
Paul Schuetz <pa.schuetz@web.de>
2022-02-11 19:17:06 +0100

    Don't allow a change of originalStartingState
    The var originalStartingState gets changed into a constant, so that it can't get
    changed or overwritten and it points still to the solution of this module.
    
    Signed-off-by: Paul Schuetz <pa.schuetz@web.de>

--- Sources/CPU-Simulation-Lib/CPUStandardVars.swift
+++ Sources/CPU-Simulation-Lib/CPUStandardVars.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class CPUStandardVars {
     public static var startingState: StateBuilder = originalStartingState
-    public static var originalStartingState: StateBuilder = StateBuilder(HoldState.init)
+    public static let originalStartingState: StateBuilder = StateBuilder(HoldState.init)
     
     public static func resetStartingState() {
         startingState = originalStartingState
